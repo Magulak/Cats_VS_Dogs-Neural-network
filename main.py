cd6719c74878167e0e9e243e8788e45883fe838c
Czarek dom <zdrojewski.czarek@gmail.com>
2023-03-15 20:47:10 +0100

    before major changes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # https://www.kaggle.com/c/dogs-vs-cats
 import tensorflow
-import scipy
-import numpy
-import os
 
-# TODO you can use Keras to convert img to array
-from PIL import Image
+# def get_available_devices():
+#     local_device_protos = device_lib.list_local_devices()
+#     return [x.name for x in local_device_protos]
+#
+#
+# print(get_available_devices())
+# print(tensorflow.test.is_built_with_cuda())
+# my output was => ['/device:CPU:0']
+# good output must be => ['/device:CPU:0', '/device:GPU:0']
+
+
 
 dir_path = r'G:/Scripts/Cats_VS_Dogs/kagglecatsanddogs_5340/PetImages'
 
@@ -22,6 +28,7 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=32,  # generate batches of 32 images
     class_mode='binary',  # binary classification problem (e.g. cats vs dogs)
     subset='training'  # specify the subset as training
+
 )
 validation_generator = train_datagen.flow_from_directory(
     directory=dir_path,
@@ -31,28 +38,24 @@ validation_generator = train_datagen.flow_from_directory(
     subset='validation'  # specify the subset as validation
 )
 
-# Create a model and compile it
-model = tensorflow.keras.models.Sequential()
-model.add(tensorflow.keras.layers.Flatten())  # 28x28[2D] needs to be converted to [1D]
-model.add(
-    tensorflow.keras.layers.Dense(252,
-                                  activation=tensorflow.nn.relu))  # Use nn.relu as default and then change it to
-# other layers
-model.add(tensorflow.keras.layers.Dense(64, activation=tensorflow.nn.relu))
-model.add(tensorflow.keras.layers.Dense(10, activation=tensorflow.nn.softmax))  # Use softmax for probability
-
+model = tensorflow.keras.Sequential([
+    tensorflow.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    tensorflow.keras.layers.MaxPooling2D((2, 2)),
+    tensorflow.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tensorflow.keras.layers.MaxPooling2D((2, 2)),
+    tensorflow.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tensorflow.keras.layers.MaxPooling2D((2, 2)),  # Myślę, że powinna się zmniejszać ilość neuronów a nie zwiększać
+    tensorflow.keras.layers.Flatten(),
+    tensorflow.keras.layers.Dense(128, activation='relu'),
+    tensorflow.keras.layers.Dense(1, activation='sigmoid')
+])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-batch_size = 64
-for e in range(1):
-    for i, (x, y) in enumerate(train_generator):
-        try:
-            model.fit(train_generator, epochs=1,steps_per_epoch=len(train_generator))
-        except tensorflow.keras.preprocessing.image.UnidentifiedImageError as e:
-            print(f"UnidentifiedImageError in epoch {e + 1}, batch {i + 1}: {e}")
-            print(f"Filename: {train_generator.filenames[i * batch_size]}")
-            continue
+batch_size = 128
+with tensorflow.device('/GPU:0'):
+    model.fit(train_generator, epochs=3, steps_per_epoch=len(train_generator))
 
 val_loss, val_acc = model.evaluate(validation_generator)
-model.save('num_reader')
+model.save('CvD ver 1') # TODO add more model info to name
+# TODO ADD SLEEP FOR 30 min after training
 print(val_loss, val_acc)
 # predictions = model.predict([x_test])
